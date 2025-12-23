a238130893b4bcea5aba41316923b507f37781c4
antlerjely96 <antlerjely96@gmail.com>
2025-12-23 23:01:51 +0700

    Fixed SpecialDays

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -87,7 +87,8 @@ export default function EventCard({
         [19, 20, 21, 22, 23, 24, 25],
         [26, 27, 28, 29, 30, 31, null]
     ];
-    const specialDays = [8, 9, 10];
+    // const specialDays = [8, 9, 10];
+    const specialDays = [10];
 
     // --- KHỐI BẢN ĐỒ ---
     const MapSection = (
